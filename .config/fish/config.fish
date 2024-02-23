dce4625aff2ff73207b0f6ba7e308bc17d86f994
twohexed <2hexed@protonmail.com>
2024-02-23 15:13:30 +0000

    added stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,35 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# set -U fish_greeting "><'>"
-set -U fish_greeting ""
-set --global fish_handle_reflow 1
+set -U fish_greeting
 
 if test -f ~/.cache/wal/sequences
-	cat ~/.cache/wal/sequences
+    cat ~/.cache/wal/sequences
 end
 
-cat /etc/motd
+function textupload
+    if string length -q $argv[1]
+        set FILE_TO_UPLOAD $argv[1]
+    else
+        set FILE_TO_UPLOAD (echo '-')
+    end
+    curl -F"file=@$FILE_TO_UPLOAD" 0x0.st
+end
+
+function wallpaper
+    if string length -q "$argv[1]"
+        if test -e "$argv[1]"
+            hyprctl hyprpaper preload "$argv[1]"
+            hyprctl hyprpaper wallpaper "eDP-1,$argv[1]"
+            wal -n -t -i "$argv[1]"
+        else if test -e "./$argv[1]"
+            hyprctl hyprpaper preload "./$argv[1]"
+            hyprctl hyprpaper wallpaper "eDP-1,./$argv[1]"
+            wal -n -t -i "$argv[1]"
+        else
+            echo "Specified wallpaper does not exist."
+        end
+    else
+        echo "Specify a wallpaper path."
+    end
+end
